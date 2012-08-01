f67cbadc99376abe85f7fe05631eab45ed44391a
wistery-k <wistery@ubuntu.(none)>
2012-08-01 04:51:52 -0700

    RMagick, blob version

--- web.rb
+++ web.rb
@@ -1,6 +1,43 @@
 require 'sinatra'
-require './haigacat'
+require 'RMagick'
 
-get '/' do
-  Haigacat.decode("123m456s789p").to_s
+def decode s
+    
+  ans = []
+  
+  for i in 0..s.length-1 do
+    next if not s[i] =~ /[0-9]/
+    c = 'not found'
+    for j in i..s.length-1 do
+      if s[j] =~ /[mpsz]/ then
+        c = s[j]
+        break
+      end
+    end
+    if c == 'not found' then
+      return []
+    end
+    ans << s[i] + c
+  end
+  
+  return ans
+  
+end
+
+def convert(hs)
+  img = Magick::ImageList.new
+  hs.each do |h|
+    img << Magick::Image.read("gif/#{h}.gif")[0]
+  end
+  img.append(false).to_blob
+end
+
+get '/*' do
+  hs = decode(params[:splat][0])
+  if hs.length == 0 then
+    400
+  else
+    content_type "image/gif"
+    convert(hs)
+  end
 end
